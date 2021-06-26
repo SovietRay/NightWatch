dd8d8753e91d9ea5fe6f8a9772bfbd70b190b193
Mikhail <m.klishevich@life-pay.ru>
2021-06-26 18:47:18 +0700

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Mikhail Klishevich on 26.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
