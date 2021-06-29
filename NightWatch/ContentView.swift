a77c6a2501a238bd6e6d035279c8d51660ce544e
Mikhail Klishevich <x-ray@mail.ru>
2021-06-30 03:21:28 +0700

    Update ContentView.swift
    Work with List

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -7,86 +7,60 @@
 
 import SwiftUI
 
+let nightlyTasks = [
+    "Check all windows",
+    "Check all doors",
+    "Document \"Strange and unusual\" occurrences",
+]
+
+let weeklyTasks = [
+    "Steal all windows",
+    "Steal all doors",
+    "Burn document \"Strange and unusual\"",
+]
+
+let monthlyTasks = [
+    "Place all windows back",
+    "Place all doors back",
+    "Document \"Strange and unusual\"?",
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    //MARK: Nightly Tasks
-                    //TODO: Need more minerals
-                    //FIXME: Fix at all
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "moon.stars"))
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Nightly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(.heavy)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                        }
-                        Text("Check all windows")
-                        Text("Check all doors")
-                        Text("Document \"Strange and unusual\" occurrences")
-                        Text("Check all windows")
-                        Text("Check all doors")
-                        Text("Document \"Strange and unusual\" occurrences")
-                        Divider();
-                    }
-                    //MARK: Weekly Tasks
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "sunset"))
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Weekly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(.heavy)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .padding(.top)
-                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                        }
-                        Text("Check all windows")
-                        Text("Check all doors")
-                        Text("Document \"Strange and unusual\" occurrences")
-                        Text("Check all windows")
-                        Text("Check all doors")
-                        Text("Document \"Strange and unusual\" occurrences")
-                        Divider();
-                    }
-                    //MARK: Monthly Tasks
-                    Group {
-                        HStack {
-                            Text(Image(systemName: "calendar"))
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .font(.title3)
-                                .fontWeight(.heavy)
-                            Text("Monthly Tasks")
-                                .underline()
-                                .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
-                                .fontWeight(.heavy)
-                                .foregroundColor(/*@START_MENU_TOKEN@*/.yellow/*@END_MENU_TOKEN@*/)
-                                .padding(.top)
-                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                        }
-                        Text("Check all windows")
-                        Text("Check all doors")
-                        Text("Document \"Strange and unusual\" occurrences")
-                        Text("Check all windows")
-                        Text("Check all doors")
-                        Text("Document \"Strange and unusual\" occurrences")
-                    }
-                }
-                Spacer()
+        List {
+            //MARK: Nightly Tasks
+            Section(header: HStack {
+                Image(systemName: "moon.stars")
+                Text("Nightly Tasks")
             }
-            .padding(.all)
-            .foregroundColor(/*@START_MENU_TOKEN@*/.gray/*@END_MENU_TOKEN@*/)
-            Spacer()
+            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)) {
+                ForEach(nightlyTasks, id: \.self, content: {
+                    taskName in
+                    Text(taskName)
+                })
+            }
+            //MARK: Weekly Tasks
+            Section(header: HStack {
+                Image(systemName: "sunset")
+                Text("Weekly Tasks")
+            }
+            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)) {
+                ForEach(weeklyTasks, id: \.self, content: {
+                    taskName in
+                    Text(taskName)
+                })
+            }
+            //MARK: Monthly Tasks
+            Section(header: HStack {
+                Image(systemName: "calendar")
+                Text("Monthly Tasks")
+            }
+            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)) {
+                ForEach(monthlyTasks, id: \.self, content: {
+                    taskName in
+                    Text(taskName)
+                })
+            }.listStyle(GroupedListStyle()) // Этот стиль был изначально для List?
         }
     }
 }
